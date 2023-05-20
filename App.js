334dc90ff8f780dcb7c7cddd65cd12a14d862009
shakesber89 <jesuinmit@gmail.com>
2023-05-19 20:33:19 -0400

    ryan

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default class App extends React.Component {
             style: { color: '#fff', fontSize: 20 },
           }}
         />
-
+          <Text>Ryan</Text>
         <Image
           style={styles.imageIcon}
           source={{
